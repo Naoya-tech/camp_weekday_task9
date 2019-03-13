c711005f07385b8484758da50820800e48e3d882
Naoya-tech <naoya.techwork@gmail.com>
2019-03-13 11:34:25 +0900

    登録ページを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,18 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all.order(created_at: "desc").limit(5)
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:notice] = "登録に成功しました。"
+      redirect_to @task
+    else
+      render "new"
+    end
+  end
 end

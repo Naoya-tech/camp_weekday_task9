1b12bb639daa6c0139dcfddf2f51a915a2fe2fde
Naoya-tech <naoya.techwork@gmail.com>
2019-03-13 11:35:35 +0900

    編集ページを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,8 +3,16 @@ class TasksController < ApplicationController
     @tasks = Task.all.order(created_at: "desc").limit(5)
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
   def new
     @task = Task.new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
   end
 
   def create
@@ -16,4 +24,23 @@ class TasksController < ApplicationController
       render "new"
     end
   end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:notice] = "タスクを更新しました。"
+      redirect_to @task
+    else
+      render "edit"
+    end
+  end
+  
+  private
+
+  def task_params # ストロングパラメータを定義する
+    params.require(:task).permit(:title, :memo, :status, :is_display)
+  end
+
+
+
 end
